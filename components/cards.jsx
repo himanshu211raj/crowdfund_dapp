9940a5383450a895212bd6ccf8dffb28dd118f9a
Himanshu Raj <himanshu211raj@gmail.com>
2022-01-03 12:48:16 +0530

    init

---
+++ components/cards.jsx
@@ -0,0 +1,116 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import LinearProgress from "@mui/material/LinearProgress";
+import { styled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import { Link } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+export default function BasicCard({
+  title,
+  time,
+  description,
+  status,
+  totalAmount,
+  amountRaised,
+}) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  return (
+    <>
+      <Card sx={{ Width: 275 }} className="my-3">
+        <CardContent>
+          <Typography className="font-bold text-3xl">{title}</Typography>
+          <Typography noWrap component="div">
+            {description}
+          </Typography>
+          <Link className="cursor-pointer" onClick={() => setIsOpen(true)}>
+            Show more
+          </Link>
+          <Typography className="py-2">Duration:{time}</Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Status: {status}
+          </Typography>
+          <Typography variant="body2" className="font-bold">
+            Goal Amount {totalAmount} ETH
+          </Typography>
+          <div className="flex items-center justify-between p-5">
+            <TextField
+              id="outlined-number"
+              label="Amount (in ETH)"
+              type="number"
+              className="mt-5"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <Button variant="outlined" className="mr-10 align-float-center">
+              Fund
+            </Button>
+          </div>
+
+          <div className="flex items-center justify-center px-5">
+            <Typography
+              className="text-center font-semibold"
+              sx={{ width: "10%" }}
+            >
+              {amountRaised} ETH
+            </Typography>
+            <LinearProgress
+              className="mx-20 my-4"
+              variant="determinate"
+              value={(amountRaised / totalAmount) * 100}
+              sx={{
+                borderRadius: "10px",
+                width: "80%",
+                height: "10px",
+                marginTop: "5px",
+                backgroundColor: "rgba(0,0,0,0.1)",
+              }}
+            />
+            <Typography className="font-semibold">{totalAmount} ETH</Typography>
+          </div>
+        </CardContent>
+      </Card>
+      {isOpen && (
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle>{title}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>{description}</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} autoFocus>
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
+    </>
+  );
+}
---
+++ components/dialog.jsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function FormDialog({ open, handleClose, handleSubmit }) {
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Project creation form</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To create a new project to be funded, please fill out the form.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Title"
+            type="title"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Description"
+            type="description"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            id="outlined-number"
+            label="Amount (in ETH)"
+            type="number"
+            className="mt-5"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            id="outlined-number"
+            label="Duration (in days)"
+            type="number"
+            className="mt-5"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Create</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
